d02c2d6881343ed18c20bb5b5ecbe83f08d84a8e
Angular CLI <angular-cli@angular.io>
2017-03-03 11:10:35 +0100

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Dw05Page } from './app.po';
+
+describe('dw05 App', () => {
+  let page: Dw05Page;
+
+  beforeEach(() => {
+    page = new Dw05Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class Dw05Page {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
