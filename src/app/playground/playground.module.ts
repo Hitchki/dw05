0301c257aa526062df8bb03fb89ea9cd56b0c3e4
Franz Irnberger <franz.irnberger@gmail.com>
2017-03-07 09:06:59 +0100

    shared modules added

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PlaygroundComponent} from './playground.component';
 import { PlaygroundRoutingModule } from './playground-routing.module';
 import { AngularFireComponent } from './angular-fire/angular-fire.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
 
+    SharedModule,
     PlaygroundRoutingModule
   ],
   declarations: [
