3678e0928749be96787ea32319e903702e443394
Franz Irnberger <franz.irnberger@gmail.com>
2017-03-16 14:16:35 +0100

    Observable.prototype.debug added

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,38 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Observable } from 'rxjs/Observable';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+const debuggerOn = true;
+
+Observable.prototype.debug = function(message: string) {
+  return this.do(
+    nextValue => {
+      if (debuggerOn) {
+        console.log(message, nextValue);
+      }
+    },
+    error => {
+      if (debuggerOn) {
+        console.error(message, error);
+      }
+    },
+    () => {
+      if (debuggerOn) {
+        console.error('Observable completed - ', message);
+      }
+    }
+  );
+};
+
+declare module 'rxjs/Observable' {
+  interface Observable<T> {
+    debug: (...any) => Observable<T>;
+  }
+}
