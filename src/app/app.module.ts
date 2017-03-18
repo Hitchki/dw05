9f94e25df1ba75f969b04f539659f33601b5d28d
Franz Irnberger <franz.irnberger@gmail.com>
2017-03-18 21:09:03 +0100

    rootReducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -88,8 +88,8 @@ import { storeFreeze } from 'ngrx-store-freeze'
     // StoreModule.provideStore(storeReducer, INITIAL_APPLICATION_STATE),
 
     // ES6 abbreviated syntax
-    // StoreModule.provideStore(rootReducer, INITIAL_APPLICATION_STATE),
-    StoreModule.provideStore(combineReducers({uiState, storeData}), INITIAL_APPLICATION_STATE),
+    StoreModule.provideStore(rootReducer, INITIAL_APPLICATION_STATE),
+    // StoreModule.provideStore(combineReducers({uiState, storeData}), INITIAL_APPLICATION_STATE),
 
     // StoreModule.provideStore(compose(storeFreeze, combineReducers)({uiState, storeData}), INITIAL_APPLICATION_STATE),
     // StoreModule.provideStore(compose(storeFreeze, combineReducers)({uiState,storeData, router: routerReducer}), INITIAL_APPLICATION_STATE),
--- src/app/store/reducers/rootReducer.ts
+++ src/app/store/reducers/rootReducer.ts
@@ -1,11 +1,23 @@
 import {compose} from '@ngrx/core';
-import {combineReducers} from '@ngrx/store';
+import { combineReducers, Action } from '@ngrx/store';
 import {uiState} from './uiStateReducer';
 import {storeData} from './storeDataReducer';
+import { ApplicationState, INITIAL_APPLICATION_STATE } from '../application-state'
 
+export function rootReducer(
+    state: ApplicationState = INITIAL_APPLICATION_STATE,
+    action: Action): ApplicationState  {
+  return {
+    uiState: uiState(state.uiState, action),
+    storeData: storeData(state.storeData, action)
+  };
+}
+
+
+/*
 export function rootReducer() {
   return compose(combineReducers)({
     uiState: uiState,
     storeData: storeData
   });
-}
+}*/
