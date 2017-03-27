9093d3e74aaf5a26c57bf818dca5198fd2726962
Franz Irnberger <franz.irnberger@gmail.com>
2017-03-27 15:31:46 +0200

    content.service removed unneccsary junk

--- src/app/cpf/content/content.service.ts
+++ src/app/cpf/content/content.service.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
 
 import { FirebaseService } from '../../core/firebase.service';
-import { PathNodesStringsHelpers } from './content-service-helpers.interface';
-import { PathNodes, PathNode } from './content.interfaces';
 
 @Injectable()
 export class ContentService {
-
-  private pathNodesSubject = new Subject<PathNodes>();
 
   constructor(
     private firebaseService: FirebaseService
